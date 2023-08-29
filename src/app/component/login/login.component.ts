49bf70f48153c245624e9568316f2ee5c1b5dfb5
Nawho <lanasso8912@gmail.com>
2023-08-29 00:32:21 -0300

    a

---
+++ src/app/component/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  formLogin: FormGroup;
+
+  constructor(
+    private router: Router
+  ) {
+    this.formLogin = new FormGroup({
+      email: new FormControl(),
+      password: new FormControl()
+    })
+  }
+
+  ngOnInit(): void {
+  }
+  menu(){
+    const emailValue = this.formLogin.get('email')?.value;
+    const passwordValue = this.formLogin.get('password')?.value;
+
+    if (emailValue && passwordValue) {
+      this.router.navigate(['/menu']);
+    }
+    else{
+      alert("Ingrese sus datos")
+    }
+  }
+}
